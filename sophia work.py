1c7722820ee72fabe42b4ac22873917b0271f4ff
sophiaezeala <134697421+sophiaezeala@users.noreply.github.com>
2023-05-26 03:54:14 -0700

    First Commit
    First Work

---
+++ sophia work.py
@@ -0,0 +1,27 @@
+about_a_person={'first_name':'praise','last_name':'ambrose','age':27,'city':'portharcourt',}
+print(about_a_person)
+print(about_a_person['first_name'])
+print(about_a_person['last_name'])
+print(about_a_person['age'])
+print(about_a_person['city'])
+
+# Arrange items in a lists
+favourite_numbers = {'hephzibah': 3, 'michael': 1, 'sophia': 36, 'anthony': 40, 'gift': 29}
+print(favourite_numbers['hephzibah'])
+print(favourite_numbers['michael'])
+print(favourite_numbers['sophia'])
+print(favourite_numbers['anthony'])
+print(favourite_numbers['gift'])
+
+glossary={'concantanation':'Operation of joining characters strings end-to end\n',
+          'tuples':'Immutable as opposed to lists which are mutable\n',
+          'list':'Used to store the sequence of various types of data\n',
+          'function':'It is a block of code which only runs when it is called\n',
+          'dictionary':'Refers to a hashed structure of various pairs of keys and values\n'
+          }
+print(glossary)
+print(glossary['concantanation'])
+print(glossary['tuples'])
+print(glossary['list'])
+print(glossary['function'])
+print(glossary['dictionary'])
